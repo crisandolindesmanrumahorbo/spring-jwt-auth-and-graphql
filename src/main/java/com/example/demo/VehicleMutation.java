85c7c397c1f11fc3ffa5bb88448b120d5685734e
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2020-09-18 10:07:01 +0700

    git hooks example pre push

--- src/main/java/com/example/demo/VehicleMutation.java
+++ src/main/java/com/example/demo/VehicleMutation.java
@@ -2,12 +2,7 @@ package com.example.demo;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Component
 public class VehicleMutation implements GraphQLMutationResolver {
